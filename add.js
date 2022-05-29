fefd3b46f1367a3de4a83ad3b4aff7d078efbc14
SwatiBadole <swatibadole1997@gmail.com>
2022-05-28 22:50:07 -0700

    Added:Ability to check employee absent or present

---
+++ add.js
@@ -0,0 +1,25 @@
+let x,y,z;
+x=10;
+y=20;
+z=x+y;
+
+console.log("addition is ", z);
+
+let person = "swati";
+let person1="swati";
+console.log(person);
+console.log(person1);
+
+a=5; b=6; c=a+b; 
+console.log(c);
+
+var x1,y1;
+x1=5+6;
+y1=x1*10;
+console.log(y1);
+
+//using var
+var x2=12;
+var y2=23;
+var z2=x2+y2;
+console.log("Addition is"+ z2);
